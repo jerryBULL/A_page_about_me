389ef749db60ea4f91e33e4bd1ace34c5bc309c1
JerryBULL <65128468+jerryBULL@users.noreply.github.com>
2020-07-06 00:50:48 +0800

    Updata
    1.RWD調整
    2.測試手機scroll功能

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
     let page=0;
     let moveing=0;
+    let scroll=0;
     const src=["home","about","skill","personal_project","contact"];
     //偵測滑鼠滾輪
     $( window ).mousewheel(function(e) {
         $("html,body").stop();
-        if(moveing===0)
+        if(moveing===0 && scroll===0)
         {   
-            moveing=1; 
+            moveing=1;
             if(e.deltaY<0)
             {   
                 if(page<4)
                 {
-                    page++;
+                    if(page===2)
+                    {
+                        scroll=1;
+                    }
+                    else
+                    {
+                        page++;
+                    }
                 }
             }
             else
@@ -28,14 +36,21 @@ $(document).ready(function() {
     //偵測使用手機手指滑動動作
     $("#home,#about,#skill,#personal_project,#contact").swipe({
         swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-            if(moveing===0)
+            if(moveing===0 && scroll===0)
             {   
                 moveing=1; 
                 if(direction === "up")
                 {   
                     if(page<4)
                     {
-                        page++;
+                        if(page===2)
+                        {
+                            scroll=1;
+                        }
+                        else
+                        {
+                            page++;
+                        }
                     }
                 }
                 else if(direction === "down")
@@ -51,6 +66,18 @@ $(document).ready(function() {
         threshold:100,
         fingers:'all'
     });
+    //skill頁面滾輪滑動
+    $("#skill_content").on('scroll', _.debounce(function(){
+            let h = $(this).height();
+            let sh = $(this)[0].scrollHeight;
+            let st =$(this)[0].scrollTop;
+            if(Math.ceil(h+st)>=sh)
+            {
+                moveing=1;
+                page++;
+                $("html,body").animate({"scrollTop":$("#"+src[page]).offset().top},function(){$("#skill_content").scrollTop(1);moveing=0;scroll=0;});
+            }
+    },100))
     //偵測網頁大小變化
     $(window).resize(function() {
         moveing=1;
